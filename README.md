a5a8c7bf814743593cd0c97c5259788e0a933deb
swxfll <swxfll@outlook.com>
2023-02-01 23:36:34 +0800

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gala CMDB
---
+++ api-server/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello, CMDB Server API!'
+
+
+if __name__ == '__main__':
+    app.run()
