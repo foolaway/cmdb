e1fafb084e4d5bfbfba1668ab6a0aa256980f8f1
swxfll <swxfll@outlook.com>
2023-02-12 16:50:13 +0800

    udpate

--- api-server/script/install-flash.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -d /cmdb-flask ];then rm -rf /cmdb-flask/*;else mkdir /cmdb-flask;fi
-
-
-# 编译 VUE
-cd /cmdb-flask/web/
-npm install
-npm run build
-docker cp node-t1:/data/web/dist/ ./
