2602566b95f15c3d683af0d2e97194fb5f07e04c
swxfll <swxfll@outlook.com>
2023-02-12 13:47:40 +0800

    udpate

--- api-server/script/install-flash.bash
+++ api-server/script/install-flash.bash
@@ -1,11 +1,9 @@
 #!/bin/bash
 if [ -d /cmdb-flask ];then rm -rf /cmdb-flask/*;else mkdir /cmdb-flask;fi
 
-git clone https://github.com/swxfll/cmdb.git /cmdb-flask
-cd /cmdb-flask/api-server/
 
-docker rm -f cmdb-flask
-docker rmi cmdb-flask
-docker build --tag cmdb-flask .
-docker run -d --name cmdb-flask -p 5000:5000 cmdb-flask
-
+# 编译 VUE
+cd /cmdb-flask/web/
+npm install
+npm run build
+docker cp node-t1:/data/web/dist/ ./
